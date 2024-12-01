b63789c86d074adce640857b725392dc37de6664
Jared Traub <jarthom@colostate.edu>
2024-11-30 17:37:21 -0700

    Delete Old Code directory

--- Old Code/player.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-class Player:
-    def __init__(self, username, symbol, sock):
-        self.username = username
-        self.symbol = symbol
-        self.sock = sock
-        self.active = True
